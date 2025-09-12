0d024e55c85d491bf1b3af225b43030148c02ad9
junseokPP <pjs981130@gmail.com>
2025-09-12 12:32:19 +0900

    13강 1단계

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
 export default function Home() {
-  return <div>여기는 /posts 페이지입니다.</div>;
+  const posts = [
+    { id: 1, title: "글 1" },
+    { id: 2, title: "글 2" },
+    { id: 3, title: "글 3" },
+    { id: 4, title: "글 4" },
+    { id: 5, title: "글 5" },
+    { id: 6, title: "글 6" },
+    { id: 7, title: "글 7" },
+    { id: 8, title: "글 8" },
+    { id: 9, title: "글 9" },
+    { id: 10, title: "글 10" },
+  ];
+
+  return (
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id} : {post.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
