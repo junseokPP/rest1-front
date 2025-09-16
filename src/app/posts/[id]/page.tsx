fd0e59115f899fcf5e154a64aecff46e854d54dd
junseokPP <pjs981130@gmail.com>
2025-09-16 16:46:11 +0900

    14~20강 커밋...

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { fetchApi } from "@/lib/client";
+import { PostDto } from "@/type/post";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState<PostDto | null>(null);
+
+  useEffect(() => {
+    fetchApi(`/api/v1/posts/${id}`).then(setPost);
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col gap-8">
+        <h1>글 상세 보기</h1>
+
+        {post === null && <div>Loading...</div>}
+
+        {post !== null && (
+          <div>
+            <div>번호 : {post.id}</div>
+            <div>제목 : {post.title}</div>
+            <div>내용 : {post.content}</div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,29 +1,32 @@
 "use client";
 
+import { fetchApi } from "@/lib/client";
+import { PostDto } from "@/type/post";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+  const [posts, setPosts] = useState<PostDto[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/v1/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPosts(data);
-      });
+    fetchApi(`/api/v1/posts`).then(setPosts);
   }, []);
 
   return (
     <div className="flex flex-col gap-9">
       <h1>글 목록</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            {post.id} : {post.title}
-          </li>
-        ))}
-      </ul>
+      {posts.length === 0 && <div>Loading...</div>}
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>
+                {post.id} : {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
